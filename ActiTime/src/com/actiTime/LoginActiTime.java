97dc83e95b35e8a54cfab6ed1945574f7a95f9fd
Maruthi <maruthirajabn@gmail.com>
2022-02-19 19:50:41 +0530

    First Commit......

---
+++ ActiTime/src/com/actiTime/LoginActiTime.java
@@ -0,0 +1,16 @@
+package com.actiTime;
+
+public class LoginActiTime {
+
+	public static void main(String[] args) 
+	{
+		actiTimeLogin();
+
+	}
+	
+	static void actiTimeLogin()
+	{
+		System.out.println("Login Sucessfull");
+	}
+
+}
